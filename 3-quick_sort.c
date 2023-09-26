1e9db0241bf3ac404c4ff9eca3e6a3e35828acbe
Gabogogi <bencitymaina@gmail.com>
2023-09-26 12:36:26 -0700

    changes to 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,78 +1,82 @@
 #include "sort.h"
 
 /**
-  * quick_sort - ...
-  * @array: ...
-  * @size: ...
-  *
-  * Return: Nothing!
-  */
-void quick_sort(int *array, size_t size)
+ * custom_quick_sort - Sorts an array of integers in ascending
+ * order using the Quick sort algorithm.
+ * @array: The array of integers to sort
+ * @size: The size of the array of integers
+ *
+ * Return: Nothing
+ */
+void custom_quick_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
 		return;
 
-	quick_sort_rec(array, 0, size - 1, size);
+	custom_quick_sort_recursive(array, 0, size - 1, size);
 }
 
 /**
-  * quick_sort_rec - ...
-  * @array: ...
-  * @lower: ...
-  * @higher: ...
-  * @size: ...
-  *
-  * Return: Nothing!
-  */
-void quick_sort_rec(int *array, int lower, int higher, size_t size)
+ * custom_quick_sort_recursive - Recursively sorts an array
+ * using the Lomuto partition scheme.
+ * @array: The array of integers to sort
+ * @low: The lower bound of the subarray to sort
+ * @high: The higher bound of the subarray to sort
+ * @size: The size of the array of integers
+ *
+ * Return: Nothing
+ */
+void custom_quick_sort_recursive(int *array, int low, int high, size_t size)
 {
-	int l_p = 0;
+	int partition_index = 0;
 
-	if (lower < higher)
+	if (low < high)
 	{
-		l_p = lomuto_partition(array, lower, higher, size);
-		quick_sort_rec(array, lower, l_p - 1, size);
-		quick_sort_rec(array, l_p + 1, higher, size);
+		partition_index = custom_lomuto_partition(array, low, high, size);
+		custom_quick_sort_recursive(array, low, partition_index - 1, size);
+		custom_quick_sort_recursive(array, partition_index + 1, high, size);
 	}
 }
 
 /**
-  * lomuto_partition - ...
-  * @array: ...
-  * @lower: ...
-  * @higher: ...
-  * @size: ...
-  *
-  * Return: Nothing!
-  */
-int lomuto_partition(int *array, int lower, int higher, size_t size)
+ * custom_lomuto_partition - Partitions an array using the Lomuto
+ * partition scheme and returns the partition index.
+ * @array: The array of integers to partition
+ * @low: The lower bound of the subarray to partition
+ * @high: The higher bound of the subarray to partition
+ * @size: The size of the array of integers
+ *
+ * Return: The partition index
+ */
+int custom_lomuto_partition(int *array, int low, int high, size_t size)
 {
-	int i = 0, j = 0, pivot = 0, aux = 0;
+	int i = 0, j = 0, pivot = 0, temp = 0;
 
-	pivot = array[higher];
-	i = lower;
+	pivot = array[high];
+	i = low;
 
-	for (j = lower; j < higher; ++j)
+	for (j = low; j < high; ++j)
 	{
 		if (array[j] < pivot)
 		{
-			aux = array[i];
+			temp = array[i];
 			array[i] = array[j];
-			array[j] = aux;
+			array[j] = temp;
 
-			if (aux != array[i])
+			if (temp != array[i])
 				print_array(array, size);
 
 			++i;
 		}
 	}
 
-	aux = array[i];
-	array[i] = array[higher];
-	array[higher] = aux;
+	temp = array[i];
+	array[i] = array[high];
+	array[high] = temp;
 
-	if (aux != array[i])
+	if (temp != array[i])
 		print_array(array, size);
 
 	return (i);
 }
+
