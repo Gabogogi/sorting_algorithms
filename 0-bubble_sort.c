81583fa0ed5a4cd02246a4781f9a04d822e07e3a
Gabogogi <bencitymaina@gmail.com>
2023-09-26 12:01:56 -0700

    changes to 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,32 +6,29 @@
   * @array: The array of integers to sort
   * @size: The size of the array of integers
   *
-  * Return: Void
+  * Return: Nothing!
   */
 void bubble_sort(int *array, size_t size)
 {
-    int temp = 0, max = 0, swapped = 1;
+	int i = 0, aux = 0, max = 0, swapped = 1;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    max = size - 1;
+	max = size - 1;
 
-    while (swapped)
-    {
-        swapped = 0;
-        for (size_t i = 0; i < max; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-                swapped = 1;
-                print_array(array, size);
-            }
-        }
-        max--;
-    }
+	for (; i < max; ++i)
+	{
+		if (array[i] > array[i + 1])
+		{
+			aux = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = aux;
+			swapped = 1;
+			print_array(array, size);
+		}
+
+		if (swapped == 1 && i == max - 1)
+			i = -1, swapped = 0, --max;
+	}
 }
-
